9c41b01f137d65cb925981d58fd8dc3432858cf2
sa17b044 <sa17b044@technikum-wien.at>
2020-11-04 00:03:49 +0100

    secound try

--- myServer.js
+++ myServer.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const cp = require("child_process");
+const multer = require("multer");
+const path = require("path");
 
+//set storage engine
+const storage = multer.diskStorage({
+  destination: "./public/upload/",
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+// Init upload
+const upload = multer({ storage: storage }).single("file");
+
+//Init app
 const app = express();
 const mongoose = require("mongoose");
 const Model = require("./models/schema");
@@ -14,8 +30,9 @@ mongoose
   .catch((err) => {
     console.log("Error" + err);
   });
-
+// EJS
 app.set("view engine", "ejs");
+// Public Folder
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
@@ -25,6 +42,7 @@ app.get("/", (req, res) => {
 app.get("/command", (req, res) => {
   res.render("command", { title: "Command" });
 });
+
 app.post("/add_log", (req, res) => {
   let cmd = req.body.cmd;
   cp.exec(cmd, (error, stdout, stderr) => {
@@ -32,14 +50,29 @@ app.post("/add_log", (req, res) => {
       console.error(`exec error: ${error}`);
       return;
     }
-    add_log_db(res,cmd,stdout);
+    add_log_db(res, cmd, stdout);
   });
 });
+app.get("/upload", (req, res) => {
+  res.render("upload", { title: "upload" });
+});
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      res.render("/upload", { msg: err });
+    } else {
+      console.log(req.file);
+      res.redirect("/logs");
+    }
+  });
+});
+
 app.get("/logs", (req, res) => {
   Model.find()
-    .sort({ createdAt: -1 }).limit(1)
+    .sort({ createdAt: -1 })
+    .limit(1)
     .then((data) => {
-      res.render("logs", { title: "Logs", data : data[0] });
+      res.render("logs", { title: "Logs", data: data[0] });
     })
     .catch((err) => {
       console.log(err);
@@ -48,18 +81,18 @@ app.get("/logs", (req, res) => {
 app.use((req, res) => {
   res.status(404).render("404", { title: "Error" });
 });
-const add_log_db= (res,cmd,log) =>{
+const add_log_db = (res, cmd, log) => {
   const model = new Model({
-    cmd : cmd,
-    log : log
+    cmd: cmd,
+    log: log,
   });
-    model
+  model
     .save()
     .then((data) => {
       // res.send(data);
-      res.redirect('/logs');
+      res.redirect("/logs");
     })
     .catch((err) => {
       console.log(err);
     });
-}
+};
